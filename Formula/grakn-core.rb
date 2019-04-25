7fbc803d7062102e3d6c3afac66135a9f112c930
Grabl <grabl@grakn.ai>
2019-04-25 19:33:00 +0000

    Update the grakn-core.rb formula to 1.5.2

--- Formula/grakn-core.rb
+++ Formula/grakn-core.rb
@@ -1,8 +1,8 @@
 class GraknCore < Formula
   desc "Grakn Core: The Knowledge Graph"
   homepage "https://grakn.ai"
-  url "https://github.com/graknlabs/grakn/releases/download/1.5.1/grakn-core-all-mac-1.5.1.zip"
-  sha256 "fd14ff53fe5babfa9100154b5f49c23f1b5ceb3f1bd48d09202ba33af2ae4a93"
+  url "https://github.com/graknlabs/grakn/releases/download/1.5.2/grakn-core-all-mac-1.5.2.zip"
+  sha256 "dd372b7fb783a9c9fb9421e392132cfb9b19405df95f96ef74e81961b0b36709"
 
   bottle :unneeded
 
