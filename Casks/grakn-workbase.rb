9fe19b85b617ef56f04f6dd5a32402bb7cd77ffb
Grabl <grabl@grakn.ai>
2020-06-09 13:48:59 +0000

    Update the grakn-workbase.rb formula to 1.2.9

--- Casks/grakn-workbase.rb
+++ Casks/grakn-workbase.rb
@@ -1,8 +1,8 @@
 cask 'grakn-workbase' do
-  version '1.2.8'
-  sha256 '9c68e08f193a3f43759102e22eb99e936ded93a5b7c6aefda8f353c886732cdd'
+  version '1.2.9'
+  sha256 '45e3ace08855f92690aaed99e82a1bf65d0cc4396b01f2ce7c83f117b7a1ed14'
 
-  url "https://github.com/graknlabs/workbase/releases/download/1.2.8/grakn-workbase-mac-1.2.8.dmg"
+  url "https://github.com/graknlabs/workbase/releases/download/1.2.9/grakn-workbase-mac-1.2.9.dmg"
   name 'Grakn Workbase'
   homepage 'https://grakn.ai/'
 
