1403954654a03f66bc8f4332febb2276a21f7b6a
Max Vorobev <vmax0770@gmail.com>
2021-01-07 16:03:37 +0300

    Revert "Update the grakn-core.rb formula to 2.0.0-alpha"
    This reverts commit 19dd569a2858297886647eb845835d39e3f72bb2.

--- Formula/grakn-core.rb
+++ Formula/grakn-core.rb
@@ -1,43 +1,31 @@
-#
-# Copyright (C) 2020 Grakn Labs
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
-#
-
 class GraknCore < Formula
   desc "Grakn Core: The Knowledge Graph"
   homepage "https://grakn.ai"
-  url "https://github.com/graknlabs/grakn/releases/download/2.0.0-alpha/grakn-core-all-mac-2.0.0-alpha.zip"
-  sha256 "2d013ddd1266031f9a3cb1b845afdb5fae85deaf49fb5c272df0515783ffc8f8"
+  url "https://github.com/graknlabs/grakn/releases/download/1.8.4/grakn-core-all-mac-1.8.4.zip"
+  sha256 "136b6933a643959d31d1f9c964bcb161db52a01c7f51ff7fa156335c0004ce54"
 
   bottle :unneeded
 
-  depends_on "openjdk@11"
+  depends_on :java => "1.8"
 
   def setup_directory(dir)
-    grakn_dir = var / name / dir
+    grakn_dir = var/name/dir
     grakn_dir.mkpath
-    orig_dir = libexec / "server" / dir
+    orig_dir = libexec/"server"/dir
     rm_rf orig_dir
     ln_s grakn_dir, orig_dir
   end
 
   def install
     libexec.install Dir["*"]
-    setup_directory "data"
-    setup_directory "logs"
-    bin.install libexec / "grakn"
-    bin.env_script_all_files(libexec, Language::Java.java_home_env("1.11"))
+    setup_directory "db"
+    setup_directory "logs" 
+    bin.install libexec/"grakn"
+    bin.env_script_all_files(libexec, Language::Java.java_home_env("1.8"))
+  end
+
+  test do
+    assert_match /RUNNING/i, shell_output("#{bin}/grakn server status")
   end
 end
+
