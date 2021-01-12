5f361ebf2c8213932116e0a06b78a0944a141fd2
Grabl <grabl@grakn.ai>
2021-01-12 23:05:21 +0000

    Update the grakn-core.rb formula to 2.0.0-alpha

--- Formula/grakn-core.rb
+++ Formula/grakn-core.rb
@@ -1,31 +1,43 @@
+#
+# Copyright (C) 2021 Grakn Labs
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
 class GraknCore < Formula
   desc "Grakn Core: The Knowledge Graph"
   homepage "https://grakn.ai"
-  url "https://github.com/graknlabs/grakn/releases/download/1.8.4/grakn-core-all-mac-1.8.4.zip"
-  sha256 "136b6933a643959d31d1f9c964bcb161db52a01c7f51ff7fa156335c0004ce54"
+  url "https://github.com/graknlabs/grakn/releases/download/2.0.0-alpha/grakn-core-all-mac-2.0.0-alpha.zip"
+  sha256 "1aa7f32680aed83646b64fdb20e04a14aa723684e5f48e3c956d30ba317d0f72"
 
   bottle :unneeded
 
-  depends_on :java => "1.8"
+  depends_on "openjdk@11"
 
   def setup_directory(dir)
-    grakn_dir = var/name/dir
+    grakn_dir = var / name / dir
     grakn_dir.mkpath
-    orig_dir = libexec/"server"/dir
+    orig_dir = libexec / "server" / dir
     rm_rf orig_dir
     ln_s grakn_dir, orig_dir
   end
 
   def install
     libexec.install Dir["*"]
-    setup_directory "db"
-    setup_directory "logs" 
-    bin.install libexec/"grakn"
-    bin.env_script_all_files(libexec, Language::Java.java_home_env("1.8"))
-  end
-
-  test do
-    assert_match /RUNNING/i, shell_output("#{bin}/grakn server status")
+    setup_directory "data"
+    setup_directory "logs"
+    bin.install libexec / "grakn"
+    bin.env_script_all_files(libexec, Language::Java.java_home_env("1.11"))
   end
 end
-
