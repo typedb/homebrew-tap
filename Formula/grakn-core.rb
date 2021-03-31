c23cb090af102b0422e3d9d718317637cec00210
Ganeshwara Hananda <ganeshwara@grakn.ai>
2021-03-31 23:49:20 +0100

    Revert "Update the grakn-core.rb formula to 2.0.0"
    This reverts commit 6a9016c22240b2ec74da56f5d73f5c20d71be772.

--- Formula/grakn-core.rb
+++ Formula/grakn-core.rb
@@ -18,8 +18,8 @@
 class GraknCore < Formula
   desc "Grakn Core: The Knowledge Graph"
   homepage "https://grakn.ai"
-  url "https://github.com/graknlabs/grakn/releases/download/2.0.0/grakn-core-all-mac-2.0.0.zip"
-  sha256 "e55e2c2a73170cc734535c3d66f3c9573ebac9ccee3f4d14de3546a305bbf8ee"
+  url "https://github.com/graknlabs/grakn/releases/download/2.0.0-alpha-9/grakn-core-all-mac-2.0.0-alpha-9.zip"
+  sha256 "b34a4ae0cdfeb3f935e2c52c7c5617369a1be689b6c81a4addc5311555a19128"
 
   bottle :unneeded
 
