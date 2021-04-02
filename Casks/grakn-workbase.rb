5240093a3e49594b7abdb591ade359d8486c4558
Grabl <grabl@grakn.ai>
2021-04-02 16:04:23 +0300

    Update the grakn-workbase.rb formula to 2.0.0

--- Casks/grakn-workbase.rb
+++ Casks/grakn-workbase.rb
@@ -1,8 +1,25 @@
-cask 'grakn-workbase' do
-  version '1.3.6'
-  sha256 '312aa9e74c6f2ed7c7dcd9356b8e66ee7ac21a090379f248c1824be72ab23343'
+#
+# Copyright (C) 2021 Grakn Labs
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
 
-  url "https://github.com/graknlabs/workbase/releases/download/1.3.6/grakn-workbase-mac-1.3.6.dmg"
+cask 'grakn-workbase' do
+  version '2.0.0'
+  sha256 '5761fbbba0e89de2a3294f6e02191f12d352f355d040efd1ce6f4c2773bb19b0'
+
+  url "https://github.com/graknlabs/workbase/releases/download/2.0.0/grakn-workbase-mac-2.0.0.dmg"
   name 'Grakn Workbase'
   homepage 'https://grakn.ai/'
 
